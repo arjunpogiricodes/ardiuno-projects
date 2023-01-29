73be40325018484655bae10ac69375d85b256231
ARJUN POGIRI <122292108+arjunpogiricodes@users.noreply.github.com>
2023-01-29 21:07:25 +0530

    Add files via upload

---
+++ calculator using ardiuno/FRIMWARE/calcualtor_4x4.ino
@@ -0,0 +1,105 @@
+#include <LiquidCrystal.h>
+#include <Keypad.h>  
+const int rs=5,  en =4, d4=3, d5=2,d6=1, d7=0;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys [ROWS] [COLS] = {
+  {'7', '8', '9', '/'},
+  {'4', '5', '6', '*'},
+  {'1', '2', '3', '-'},
+  {'C', '0', '=', '+'}
+};
+byte rowPins[ROWS] = {13,12,11,10};
+byte colPins[COLS] = {9,8,7,6};
+
+Keypad myKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+boolean presentValue = false;
+boolean next = false;
+boolean final = false;
+String num1, num2;
+int answer;
+char op;
+
+void setup()
+{
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print(" Arduino ");
+  lcd.setCursor(0,1);
+  lcd.print("   Calculator ");
+  delay(300);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" arjun ");
+  lcd.setCursor(0,1);
+  lcd.print(" pogiri ");
+  delay(300);
+  lcd.clear();
+}
+
+void loop(){
+  char key = myKeypad.getKey();
+
+  if (key != NO_KEY && (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0'))
+  {
+    if (presentValue != true)
+    {
+      num1 = num1 + key;
+      int numLength = num1.length();
+      lcd.setCursor(15 - numLength, 0); //to adjust one whitespace for operator
+      lcd.print(num1);
+    }
+    else 
+    {
+      num2 = num2 + key;
+      int numLength = num2.length();
+      lcd.setCursor(15 - numLength, 1);
+      lcd.print(num2);
+      final = true;
+    }
+  }
+
+  else if (presentValue == false && key != NO_KEY && (key == '/' || key == '*' || key == '-' || key == '+'))
+  {
+    if (presentValue == false)
+    {
+      presentValue = true;
+      op = key;
+      lcd.setCursor(15,0);
+      lcd.print(op);
+    }
+  }
+
+  else if (final == true && key != NO_KEY && key == '='){
+    if (op == '+'){
+      answer = num1.toInt() + num2.toInt();
+    }
+    else if (op == '-'){
+      answer = num1.toInt() - num2.toInt();
+    }
+    else if (op == '*'){
+      answer = num1.toInt() * num2.toInt();
+    }
+    else if (op == '/'){
+      answer = num1.toInt() / num2.toInt();
+    }    
+      lcd.clear();
+      lcd.setCursor(15,0);
+      lcd.autoscroll();
+      lcd.print(answer);
+      lcd.noAutoscroll();
+  }
+  else if (key != NO_KEY && key == 'C'){
+    lcd.clear();
+    presentValue = false;
+    final = false;
+    num1 = "";
+    num2 = "";
+    answer = 0;
+    op = ' ';
+  }
+}
